f303ea4cf322bc1f3cd118f41bc3aadf8c05bd09
BadinMulamba <badibangamulamba@gmail.com>
2023-09-25 17:23:22 +0200

    backtoTop

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var tabcontents = document.getElementsByClassName('tab-contents');
 
 var sidemenus = document.getElementById("sidemenu");
 
-var openmenuBtn = document.getElementById('fas')
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
 
 function opentab(tabname){
     for (tablink of tablinks){
@@ -27,3 +31,19 @@ function closemenu(){
    
 };
 
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
